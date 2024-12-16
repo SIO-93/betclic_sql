9446120f5a13ea4f70f571e34b3ddbad50df86be
Nico <boubphavong.nicolas.sio@gmail.com>
2024-12-16 17:17:58 +0100

    table users

--- create.sql
+++ create.sql
@@ -12,6 +12,12 @@ CREATE TABLE account(
     value INT
 );
 
+CREATE TABLE users (
+    id INT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
 CREATE TABLE bets (
     id INT PRIMARY KEY,             
     user_id INT,                 
