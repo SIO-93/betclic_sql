f032d3ac1b451788ecbb29c2cdf7f1fa9b728797
Abdouleloup <abdourahman.boukraa.sio@gmail.com>
2024-12-16 16:35:06 +0100

    table bets

---
+++ create.sql
@@ -0,0 +1,10 @@
+CREATE TABLE bets (
+    id INT PRIMARY KEY,             
+    user_id INT,                 
+    opponent_user_id INT,        
+    exam_id INT,       
+    amount INT,              
+    FOREIGN KEY (user_id) REFERENCES users(id),  
+    FOREIGN KEY (opponent_user_id) REFERENCES users(id),
+    FOREIGN KEY (exam_id) REFERENCES exams(id)
+);
