8e81675341dc3ba7c465f0803676fa9605041d48
kumma-270 <lago.mael.sio@gmail.com>
2024-12-16 16:41:54 +0100

    table exams et account

--- create.sql
+++ create.sql
@@ -1,3 +1,17 @@
+CREATE TABLE exams( 
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(20),
+    date INT,
+    sujet VARCHAR(30)
+    
+); 
+
+CREATE TABLE account( 
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user VARCHAR(20),
+    value INT
+);
+
 CREATE TABLE bets (
     id INT PRIMARY KEY,             
     user_id INT,                 
@@ -8,3 +22,4 @@ CREATE TABLE bets (
     FOREIGN KEY (opponent_user_id) REFERENCES users(id),
     FOREIGN KEY (exam_id) REFERENCES exams(id)
 );
+
